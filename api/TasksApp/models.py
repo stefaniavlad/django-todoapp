bfed6d72e406907848125863e27611680c84f2fb
Stefania <vladstefania98@gmail.com>
2025-03-31 22:23:23 +0300

    models

---
+++ api/TasksApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    category = models.CharField(max_length=100, blank=True, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    create_date = models.DateTimeField(auto_now=True)
+    update_date = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField()
+    status = models.CharField(default='Open')
+
+
+    def __str__(self):
+        return self.title
